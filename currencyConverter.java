0684df34a0f4e7cd80931162a02e4d7d33b8f978
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2023-02-08 18:41:49 +0000

    all import

--- currencyConverter.java
+++ currencyConverter.java
@@ -1,2 +1,12 @@
-//test
-1234
+
+package com.exchange;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.InputStream;
+import java.net.*;
+import com.google.gson.*;
+
