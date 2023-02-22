514c7488ce5bb8fad8e95812210d2d5bebde4cc2
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2023-02-22 09:16:36 +0000

    public class Convert extends HttpServlet - attributes code

--- currencyConverter.java
+++ currencyConverter.java
@@ -30,7 +30,28 @@ class Recv {
 }
 
 public class Convert extends HttpServlet {
-    
-    
-    
+    /**
+    * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+    * @param request servlet request
+    * @param response servlet response
+    * @throws ServletException if a servlet-specific error occurs
+    * @throws IOException if an I/O error occurs
+    */
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+            String query = "";
+            String amount = "";
+            String curTo = "";
+            String curFrom = "";
+            String submit = "";
+            String res  = "";
+            HttpSession session;
+            resp.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = resp.getWriter();
+            /*Read request parameters*/
+            amount = req.getParameter("amount");
+            curTo = req.getParameter("to");
+            curFrom = req.getParameter("from");
+            /*Open a connection to google and read the result*/
+        }
 }
