e7fc08d12a7e10de74bc110af1851ef0f762fb52
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2023-03-03 22:27:07 +0000

    doGet method

--- currencyConverter.java
+++ currencyConverter.java
@@ -89,6 +89,13 @@ public class Convert extends HttpServlet {
     * @param request servlet request
     * @param response servlet response
     * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs */
+    * @throws IOException if an I/O error occurs 
+    */
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            processRequest(request, response);
+        }
 
 }
