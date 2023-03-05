8b8f6409b7d6725e4c024aa816f300c0bca3b90c
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2023-03-05 22:47:13 +0000

    getServletInfo method

--- currencyConverter.java
+++ currencyConverter.java
@@ -112,4 +112,13 @@ public class Convert extends HttpServlet {
             processRequest(request, response);
         }
 
+    /**
+	* Returns a short description of the servlet.
+	* @return a String containing servlet description
+	*/
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
